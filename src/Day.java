bfb4a92332cc85c59a757219f187e0679e2da3b8
aleksei_parafiniuk <alpparaf@gmail.com>
2021-11-14 21:58:47 +0300

    class Main

--- src/Day.java
+++ src/Day.java
@@ -1,14 +1,87 @@
 public class Day {
 
-    private float timeWakeUp;
-    private float timeBreakfast;
-    private float timeSecondBreakfast;
-    private float timeSupper;
-    private float timeLunch;
-    private float timeDinner;
-    private float timeSecondDinner;
-    private float timeSleepDown;
+    private double timeWakeUp;
+    private double timeBreakfast;
+    private double timeSecondBreakfast;
+    private double timeSupper;
+    private double timeLunch;
+    private double timeDinner;
+    private double timeSecondDinner;
+    private double timeSleepDown;
 
 
-    public day()
+    public Day(double timeWakeUp, double timeBreakfast, double timeSecondBreakfast, double timeSupper, double timeLunch, double timeDinner, double timeSecondDinner, double timeSleepDown) {
+        this.timeWakeUp = timeWakeUp;
+        this.timeBreakfast = timeBreakfast;
+        this.timeSecondBreakfast = timeSecondBreakfast;
+        this.timeSupper = timeSupper;
+        this.timeLunch = timeLunch;
+        this.timeDinner = timeDinner;
+        this.timeSecondDinner = timeSecondDinner;
+        this.timeSleepDown = timeSleepDown;
+    }
+
+    public double getTimeWakeUp() {
+        return timeWakeUp;
+    }
+
+    public void setTimeWakeUp(double timeWakeUp) {
+        this.timeWakeUp = timeWakeUp;
+    }
+
+    public double getTimeBreakfast() {
+        return timeBreakfast;
+    }
+
+    public void setTimeBreakfast(double timeBreakfast) {
+        this.timeBreakfast = timeBreakfast;
+    }
+
+    public double getTimeSecondBreakfast() {
+        return timeSecondBreakfast;
+    }
+
+    public void setTimeSecondBreakfast(double timeSecondBreakfast) {
+        this.timeSecondBreakfast = timeSecondBreakfast;
+    }
+
+    public double getTimeSupper() {
+        return timeSupper;
+    }
+
+    public void setTimeSupper(double timeSupper) {
+        this.timeSupper = timeSupper;
+    }
+
+    public double getTimeLunch() {
+        return timeLunch;
+    }
+
+    public void setTimeLunch(double timeLunch) {
+        this.timeLunch = timeLunch;
+    }
+
+    public double getTimeDinner() {
+        return timeDinner;
+    }
+
+    public void setTimeDinner(double timeDinner) {
+        this.timeDinner = timeDinner;
+    }
+
+    public double getTimeSecondDinner() {
+        return timeSecondDinner;
+    }
+
+    public void setTimeSecondDinner(double timeSecondDinner) {
+        this.timeSecondDinner = timeSecondDinner;
+    }
+
+    public double getTimeSleepDown() {
+        return timeSleepDown;
+    }
+
+    public void setTimeSleepDown(double timeSleepDown) {
+        this.timeSleepDown = timeSleepDown;
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Day nov15_21 = new Day(5.00, 6.45,);
+    }
+}
