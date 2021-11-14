cfee2c2afe8d995d0b6f205d1729d1569ff0be22
aleksei_parafiniuk <alpparaf@gmail.com>
2021-11-14 15:08:41 +0300

    Day.java

---
+++ readme.txt
@@ -0,0 +1,3 @@
+14.11.2021 - I pet project Samsara.
+It would consist information about my family's everyday moments, astronomy and history detailes of the day,
+and analyze taken information. 
--- src/Day.java
+++ src/Day.java
@@ -1,3 +1,14 @@
 public class Day {
+
+    private float timeWakeUp;
+    private float timeBreakfast;
+    private float timeSecondBreakfast;
+    private float timeSupper;
+    private float timeLunch;
+    private float timeDinner;
+    private float timeSecondDinner;
+    private float timeSleepDown;
+
+
     public day()
 }
